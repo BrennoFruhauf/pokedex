12908c3d98108e1387a480f5bef710389e355b36
Brenno <brennofruhauf@outlook.com>
2022-12-17 17:35:59 -0300

    Pokedex V1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,332 @@
-import poke from './pokeapi.js'
-
-// const pokemonList = document.getElementById('pokemon-list')
-// const pokemonCards = document.getElementById('pokemon-card')
+const pokemonCards = document.getElementById('pokemon-card')
 const btnShowMore = document.getElementById('show-more')
+const pokemonDetail = document.getElementById('pokemon-details')
 const containerBtn = document.getElementById('show-pokemon')
-const limit = 16
+const divMaster = document.getElementById('master')
+const limit = 30
 let offset = 0
-const maxCard = 1154
+const maxCard = 905
 
+// Create pokemon list item
 function showPokemonHTML(pokemon) {
   return `
-    <li class="card-effect" style="background-color: ${pokemon.color}">
-    <!-- <li class="${pokemon.type} card-effect"> -->
+    <div>
+      <p class="pokemon-number">#${(pokemon.number).toLocaleString('pt-BR', {minimumIntegerDigits: 3, useGrouping: false})}</p>
+      <p class="pokemon-name">${pokemon.name}</p>
+    </div>
+    <div class="about">
       <div>
-        <p class="pokemon-number">#${(pokemon.number).toLocaleString('pt-BR', {minimumIntegerDigits: 3, useGrouping: false})}</p>
-        <p class="pokemon-name">${pokemon.name}</p>
+        ${pokemon.types.map((type) => `<span>${type}</span>`).join('')}
       </div>
-      <div class="about">
-        <div>
-          ${pokemon.types.map((type) => `<span>${type}</span>`).join('')}
-        </div>
-        <img src="${pokemon.artwork}" alt="${pokemon.name}">
-      </div>
-    </li>
+      <img src="${pokemon.artwork}" alt="${pokemon.name}">
+    </div>
   `
 }
 
-function loadPokemons(offset, limit) {
-  poke.getPokemons(offset, limit).then((pokemons = []) => {
-    const newHTML = pokemons.map(showPokemonHTML).join('')
-    const newList = document.createElement('ol')
+// Create all elements of pokemon details container and events necessary
+function showPokemonDetails(pokemon) {
 
-    newList.id = 'pokemon-list'
-    newList.innerHTML = `
-                          <ol id="pokemon-card">
-                            ${newHTML}
-                          </ol>
-                        `
-    document.body.appendChild(newList)
-    containerBtn.insertAdjacentElement('beforebegin', newList)
+  // Show stats bar of pokemon
+  pokemonDetail.addEventListener('animationstart', event => {
+    const element = event.target
+    
+    if (element.nodeName == 'DIV' && element.className == 'bar hp')
+      element.style.setProperty('--hpNumber', `${pokemon.hp / 270 * 100}%`)
+
+    if (element.nodeName == 'DIV' && element.className == 'bar atk')
+      element.style.setProperty('--atkNumber', `${pokemon.attack / 270 * 100}%`)
+
+    if (element.nodeName == 'DIV' && element.className == 'bar de')
+      element.style.setProperty('--deNumber', `${pokemon.defense / 270 * 100}%`)
+
+    if (element.nodeName == 'DIV' && element.className == 'bar sp-atk')
+      element.style.setProperty('--saNumber', `${pokemon.sa / 270 * 100}%`)
+
+    if (element.nodeName == 'DIV' && element.className == 'bar sp-de')
+      element.style.setProperty('--sdNumber', `${pokemon.sd / 270 * 100}%`)
+
+    if (element.nodeName == 'DIV' && element.className == 'bar speed')
+      element.style.setProperty('--spdNumber', `${pokemon.speed / 270 * 100}%`)
+
+    if (element.nodeName == 'DIV' && element.className == 'bar total')
+      element.style.setProperty('--totalNumber', `${(pokemon.hp + pokemon.defense + pokemon.sa + pokemon.sd + pokemon.speed) / 1410 * 100}%`)
+  })
+
+  // Show menu select of pokemon variation
+  function showVariation() {
+    if (pokemon.variation.length >= 1)
+      return `
+        <div id="variation">
+          <label for="selected">Variations: </label>
+          <div id="select-menu">
+            <span id='selected' class='selected'>${pokemon.name}</span>
+            <div id='drop-down' style="display: none;">
+              <div class='listDropDown'>
+                ${pokemon.variation.map((variation, indice) => 
+                  `<div class="item" value="${pokemon.variationId[indice]}">${variation}</div>`).join('')}
+              </div>
+            </div>
+          </div>
+        </div>`
+
+    else
+      return ''
+  }
+
+  // Show weight of pokemon
+  function showWeight(){
+    if (pokemon.weight < 1000)
+      return `${pokemon.weight} kg`
+
+    else
+      return 'Unknow'
+  }
+
+  // Show evolution chain of pokemon
+  function showChain() {
+    if (pokemon.evolutionChain.length > 1)
+      return `
+        <div id="evolution-chain">
+          <span>Evolution Chain</span>
+          <div>
+            ${pokemon.evolutionChain.map((poke, indice) => 
+              `<img class="chain" value="${pokemon.urlPokemonChain[indice]}" src="${poke}" alt="${pokemon.namePokemonChain[indice]}">`).join('')}
+          </div>
+        </div>
+      `
+    else
+      return ''
+  }
+
+  // Return of function showPokemonDetails
+  return `
+    <div>
+      <div id="body-details" class="${pokemon.type}">
+        <div style="margin: 0 2rem">
+          <span id="back" class="material-symbols-rounded back">keyboard_backspace</span>
+          <div class="info">
+            <span class="pokemon-name">${pokemon.name}</span>
+            <span class="pokemon-number">#${(pokemon.number).toLocaleString('pt-BR', {minimumIntegerDigits: 3, useGrouping: false})}</span>
+          </div>
+          <div class="type">
+            ${pokemon.types.map((type) => `<span>${type}</span>`).join('')}
+          </div>
+        </div>
+        <div class="pokemon-img">
+          <img src="${pokemon.artwork}" alt="${pokemon.name}">
+        </div>
+        <div class="content-pokemon">
+          <div>
+            ${showVariation()}
+            <div id="about">
+              <div>
+                <span>Height</span>
+                <span>${pokemon.height} m</span>
+              </div>
+
+              <div>
+                <span>Weight</span>
+                <span>${showWeight()}</span>
+              </div>
+
+              <div>
+                <span>Habitat</span>
+                <span class="text">${pokemon.habitat}</span>
+              </div>
+
+              <div>
+                <span>Shape</span>
+                <span class="text">${pokemon.shape}</span>
+              </div>
+            </div>
+
+            <div id="stats">
+              <div id="hp" class="stat">
+                <div>
+                  <span class="name-stat">HP</span>
+                  <div class="bar hp"></div>
+                </div>
+                <span class="number-stat">${pokemon.hp}</span>
+              </div>
+
+              <div id="atk" class="stat">
+                <div>
+                  <span class="name-stat">Attack</span>
+                  <div class="bar atk"></div>
+                </div>
+                <span class="number-stat">${pokemon.attack}</span>
+              </div>
+
+              <div id="de" class="stat">
+                <div>
+                  <span class="name-stat">Defense</span>
+                  <div class="bar de"></div>
+                </div>
+                <span class="number-stat">${pokemon.defense}</span>
+              </div>
+
+              <div id="sp-atk" class="stat">
+                <div>
+                  <span class="name-stat">Sp. Attack</span>
+                  <div class="bar sp-atk"></div>
+                </div>
+                <span class="number-stat">${pokemon.sa}</span>
+              </div>
+
+              <div id="sp-de" class="stat">
+                <div>
+                  <span class="name-stat">Sp. Defense</span>
+                  <div class="bar sp-de"></div>
+                </div>
+                <span class="number-stat">${pokemon.sd}</span>
+              </div>
+
+              <div id="speed" class="stat">
+                <div>
+                  <span class="name-stat">Speed</span>
+                  <div class="bar speed"></div>
+                </div>
+                <span class="number-stat">${pokemon.speed}</span>
+              </div>
+
+              <div id="total" class="stat">
+                <div>
+                  <span class="name-stat">Total</span>
+                  <div class="bar total"></div>
+                </div>
+                <span class="number-stat">${pokemon.hp + pokemon.attack + pokemon.defense + pokemon.sa + pokemon.sd + pokemon.speed}</span>
+              </div>
+            </div>
+            ${showChain()}
+          </div>
+        </div>
+      </div>
+    </div>`
+}
+
+document.body.onload = () => {
+  setTimeout(() => {
+    document.getElementById('load-box').style.display = 'none'
+    document.body.classList.remove('noscroll')
+  }, 1000)
+}
+
+// Get target element clicked of pokemon details container
+pokemonDetail.addEventListener('click', event => {
+  const element = event.target
+  const dropDown = document.getElementById('drop-down')
+  const selectMenu = document.getElementById('selected')
+
+  // Show Detail of Chain Pokemon
+  if (element.nodeName == 'IMG' && element.className == 'chain') {
+    const pokemonId = element.src.slice(0, element.src.length - 4).substr(96)
+    poke.getPokemon(pokemonId).then(result => pokemonDetail.innerHTML = showPokemonDetails(result))
+  }
+
+  // Open & close dropdown of variant pokemon list
+  if (element.id == 'selected') {
+    const dpLength = dropDown.lastElementChild.childElementCount
+
+    if (dropDown.style.display == 'none') {
+      dropDown.style.display = 'block'
+      selectMenu.style.setProperty('--rotate-arrow', 'rotate(180deg)')
+    
+      if (dpLength == 2)
+        dropDown.style.setProperty('--height-dropdown', '62px')
+      else if (dpLength > 2)
+        dropDown.style.setProperty('--height-dropdown', '92px')
+
+    } else {
+      dropDown.style.display = 'none'
+      selectMenu.style.setProperty('--rotate-arrow', 'rotate(0deg)')
+    }
+  }
+
+  // Close dropdown menu list when click out
+  if (element.id != 'selected') {
+    if (dropDown != null) {
+      dropDown.style.display = 'none'
+      selectMenu.style.setProperty('--rotate-arrow', 'rotate(0deg)')
+    }
+  }
+
+  // Show detail of variant pokemon selected
+  if (element.classList == 'item') {
+    const idPokemon = element.getAttribute('value')
+    poke.getPokemon(idPokemon).then(result => pokemonDetail.innerHTML = showPokemonDetails(result))
+  }
+
+  // Close pokemon details container
+  if (element.id == 'back') {
+    if (window.screen.width < 800) {
+      pokemonDetail.classList.remove('show-animation')
+      pokemonDetail.classList.add('close-animation')
+    } else {
+      pokemonDetail.classList.remove('show-animation')
+      pokemonDetail.classList.remove('close-animation')
+    }
+    pokemonDetail.classList.remove('show')
+    pokemonDetail.classList.add('close')
+    divMaster.style.filter = 'grayscale(0)'
+    divMaster.style.pointerEvents = 'auto'
+
+    if (window.screen.width < 800) {
+      setTimeout(() => {
+        pokemonDetail.innerHTML = ''
+        pokemonDetail.classList.add('hidden')
+        document.body.classList.remove('noscroll')
+      }, 1200)
+    } else {
+      pokemonDetail.innerHTML = ''
+      pokemonDetail.classList.add('hidden')
+      document.body.classList.remove('noscroll')
+
+    }
+  }
+})
+
+// Create each pokemon list item card 
+function loadPokemons(offset, limit) {
+  poke.getPokemons(offset, limit).then((pokemon = []) => {
+    pokemon.map(async (currentPoke) => {
+      const newLi = document.createElement('li')
+      newLi.className = `${currentPoke.type} card-effect`
+      newLi.value = currentPoke.number
+      newLi.onclick = pokemonInfo
+      newLi.innerHTML = showPokemonHTML(currentPoke)
+      pokemonCards.appendChild(newLi)
+
+      // Open pokemon details container
+      function pokemonInfo() {
+        console.log(window.screen.width)
+        const pokemonSelected = currentPoke.number
+        document.body.className = 'noscroll'
+        poke.getPokemon(pokemonSelected).then(result => pokemonDetail.innerHTML = showPokemonDetails(result))
+        pokemonDetail.classList.remove('hidden')
+        pokemonDetail.classList.remove('close')
+        pokemonDetail.classList.add('show')
+        divMaster.style.filter = 'grayscale(1)'
+        divMaster.style.pointerEvents = 'none'
+        if (window.screen.width < 800) {
+          pokemonDetail.classList.add('show-animation')
+          pokemonDetail.classList.remove('close-animation')
+        } else {
+          pokemonDetail.classList.remove('show-animation')
+          pokemonDetail.classList.remove('close-animation')
+        }
+      }
+    })
   })
 }
 
+pokemonCards.onload = () => {
+  console.log('oi')
+}
 
+// Load first list of pokemon cards items
 loadPokemons(offset, limit)
 
+// Load next list of pokemon cards items
 btnShowMore.addEventListener('click', () => {
   offset += limit
   const qtdCard = offset + limit
--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -1,10 +1,14 @@
-import ColorThief from "../modules/color-thief.js";
-
 const poke = {}
 
 class Pokemon {
   name;
-  color;
+  habitat;
+  namePokemonChain = [];
+  evolutionChain = [];
+  urlPokemonChain = [];
+  variation = [];
+  variationId = [];
+  shape;
   type;
   types = [];
   hp;
@@ -19,69 +23,110 @@ class Pokemon {
   weight;
 }
 
-const rgbToHex = (rgb, brightness = 0) => '#' + rgb.map((x) => {
-  let newColor = Number.parseInt(x * (brightness / 100 + 1))
-  if (newColor > 255) newColor = 255
-  else if (newColor < 0) newColor = 0
-  const hex = newColor.toString(16)
-  return hex.length === 1 ? '0' + hex : hex
-}).join('')
-
-async function convertPokeapiToModel(api) {
+// Convert JSON to my object model
+function convertPokeapiToModel(api) {
   const pokemon = new Pokemon()
-  const img = new Image()
-  const colorThief = new ColorThief()
-  const stats = api.stats.map((statSlot) => statSlot.base_stat)
-  const [hp, atk, de, sa, sd, speed] = stats
-  
+
   pokemon.number = api.id
-  pokemon.name = api.name
-  pokemon.height = api.height
-  pokemon.weight = api.weight
-  pokemon.hp = hp
-  pokemon.attack = atk
-  pokemon.defense = de
-  pokemon.sa = sa
-  pokemon.sd = sd
-  pokemon.speed = speed
+  pokemon.name = api.name.replace('-', ' ')
   pokemon.artwork = api.sprites.other['official-artwork'].front_default
-
-  const imgLoad = new Promise (resolve => {
-    img.crossOrigin = 'Anonymous'
-    img.src = pokemon.artwork
-    img.onload = () => {
-      if (img.complete)
-        colorThief.getColor(img).then(resolve)
-    }
-  })
-
-  pokemon.color = await imgLoad.then(colorRGB => rgbToHex(colorRGB))
   pokemon.types = api.types.map((typeSlot) => typeSlot.type.name)
   pokemon.type = pokemon.types[0]
 
   return pokemon
 }
 
-// Cria um método para fazer uma requisição ao atributo url da API
+// Get sub-url API, convert to JSON and use convertPokeapiToModel
 poke.getPokemonsDetails = (pokemon) => {
   return fetch(pokemon.url)
     .then((response) => response.json())
     .then(convertPokeapiToModel)
 }
 
-// Cria um método para obter os dados da API
+// Get url API, get list JSON of pokemons and convert to my object model
 poke.getPokemons = (offset = 0, limit = 16) => {
-  // Declaração da API Pokeapi
   const api = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
   return fetch(api)
-    // Converte os dados da resposta para um arquivo json
     .then((response) => response.json())
-    // Acessa o atributo results do arquivo json
     .then((jsonBody) => jsonBody.results)
     .then((pokemons) => pokemons.map(poke.getPokemonsDetails))
     .then((detailRequest) => Promise.all(detailRequest))
     .then((pokemonsDetails) => pokemonsDetails)
 }
 
-export default poke
+// Get all necessary about one pokemon
+poke.getPokemon = async (idPokemon) => {
+  const pokemon = new Pokemon()
+  const pokemonLink = `https://pokeapi.co/api/v2/pokemon/${idPokemon}/`
+  
+  await fetch(pokemonLink)
+  .then(response => response.json())
+  .then(async pokemonInfo => {
+    const stats = pokemonInfo.stats.map((statSlot) => statSlot.base_stat)
+    const [hp, atk, de, sa, sd, speed] = stats
+
+    pokemon.name = pokemonInfo.name.replace(/[-]/g, ' ')
+    pokemon.number = pokemonInfo.id
+    pokemon.height = pokemonInfo.height / 10
+    pokemon.weight = pokemonInfo.weight / 10
+    pokemon.hp = hp
+    pokemon.attack = atk
+    pokemon.defense = de
+    pokemon.sa = sa
+    pokemon.sd = sd
+    pokemon.speed = speed
+    pokemon.artwork = pokemonInfo.sprites.other['official-artwork'].front_default
+    pokemon.types = pokemonInfo.types.map((typeSlot) => typeSlot.type.name)
+    pokemon.type = pokemon.types[0]
+
+    await fetch(pokemonInfo.species.url)
+    .then(secondResponse => secondResponse.json())
+    .then(async secondPokemonInfo => {
+      let indiceNumber
+      secondPokemonInfo.habitat ? pokemon.habitat = secondPokemonInfo.habitat.name.replace(/[-]/g, ' ') : pokemon.habitat = 'Unknown'
+      pokemon.shape = secondPokemonInfo.shape.name
+      pokemon.variation = secondPokemonInfo.varieties.map((variant, indice) => {
+        if (pokemon.name != variant.pokemon.name.replace(/[-]/g, ' '))
+          return variant.pokemon.name.replace(/[-]/g, ' ')
+        else
+          indiceNumber = indice
+      })
+      pokemon.variationId = secondPokemonInfo.varieties.map((variant, indice) => {
+        if (indice != indiceNumber)
+          return variant.pokemon.url.slice(0, variant.pokemon.url.length - 1).substr(34)
+      })
+      pokemon.variation.splice(indiceNumber, 1)
+      pokemon.variationId.splice(indiceNumber, 1)
+      
+      if (secondPokemonInfo.evolution_chain != null) {
+        await fetch(secondPokemonInfo.evolution_chain.url)
+        .then(thirdResponse => thirdResponse.json())
+        .then(thirdPokemonInfo => {
+          let firstName = '', firstUrl = '', secondName = '', secondUrl = '', thirdName = '', thirdUrl = '', x = 0
+          
+          if (thirdPokemonInfo.chain.species)
+          firstName = thirdPokemonInfo.chain.species.name, firstUrl = thirdPokemonInfo.chain.species.url
+          
+          if (thirdPokemonInfo.chain.evolves_to != '') {
+            secondName = thirdPokemonInfo.chain.evolves_to['0'].species.name, secondUrl = thirdPokemonInfo.chain.evolves_to['0'].species.url
+            
+            if (thirdPokemonInfo.chain.evolves_to['0'].evolves_to != '')
+            thirdName = thirdPokemonInfo.chain.evolves_to['0'].evolves_to['0'].species.name, thirdUrl = thirdPokemonInfo.chain.evolves_to['0'].evolves_to['0'].species.url
+          }
+          
+          const arrayUrl = [firstUrl, secondUrl, thirdUrl], arrayName = [firstName, secondName, thirdName]
+          for (let i = 0; i < 3; i++)
+            if (arrayUrl[i] != '') {
+              pokemon.namePokemonChain[x] = arrayName[i]
+              pokemon.evolutionChain[x] = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${arrayUrl[i].slice(0, arrayUrl[i].length - 1).substr(42)}.png`
+              pokemon.urlPokemonChain[x] = arrayUrl[i].slice(0, arrayUrl[i].length - 1).substr(42)
+              x++
+            }
+        })
+      }
+    })
+  })
+
+  return pokemon
+}
